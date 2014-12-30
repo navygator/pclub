c5a76c39890ffacb7bf4e42608b587fde45635a9
navygator1 <navygator@live.ru>
2014-12-30 08:53:56 +0000

    add user index

---
+++ .c9/metadata/preview-https:/pclub-navygator1.c9.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://pclub-navygator1.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://pclub-navygator1.c9.io","previewer":"preview.browser"},"timestamp":1419926953822,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :load_resource
+  before_action :check_user, only: [:edit, :update, :destroy]
   def index
   end
 
@@ -15,8 +17,31 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @resource.present? && @resource.update_attributes(user_params)
+      redirect_to users_path, notice: "Successfuly update user"
+    else
+      flash[:alert] = "Can't update user"
+      render 'edit'
+    end
   end
 
   def destroy
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
+  
+  def load_resource
+    if params[:action] == 'index'
+      @collection = User.all
+    else
+      @resource = User.find(params[:id])
+    end
+  end
+  
+  def check_user
+    redirect_to users_path, alert: "Access denied!" if current_user != @resource
   end
 end
